1f1f41eddae4edaf5d6130a81d457787096a0c9e
Stavros <schampilomatis@gmail.com>
2025-09-21 16:53:17 +0200

    start tcp server

--- src/main.zig
+++ src/main.zig
@@ -1,15 +1,16 @@
 const std = @import("std");
 const reader = @import("core.zig");
+const tcp = @import("tcp.zig");
 
 fn hello() void {
     while (true) {
         std.Thread.sleep(1 * std.time.ns_per_s);
         std.debug.print("Hello", .{});
-        reader.foo();
     }
 }
 
 pub fn main() !void {
+    _ = try tcp.run_server();
     var thread = try std.Thread.spawn(.{}, hello, .{});
     thread.join();
 }
---
+++ src/tcp.zig
@@ -0,0 +1,53 @@
+const std = @import("std");
+const posix = std.posix;
+const net = std.net;
+// https://www.openmymind.net/TCP-Server-In-Zig-Part-1-Single-Threaded/
+
+pub fn run_server() !void {
+    const address = try net.Address.parseIp("127.0.0.1", 8081);
+    const listener = try posix.socket(address.any.family, posix.SOCK.STREAM, posix.IPPROTO.TCP);
+    defer posix.close(listener);
+
+    try posix.setsockopt(listener, posix.SOL.SOCKET, posix.SO.REUSEADDR, &std.mem.toBytes(@as(c_int, 1)));
+    try posix.bind(listener, &address.any, address.getOsSockLen());
+    try posix.listen(listener, 128);
+
+    var buf: [128]u8 = undefined;
+    while (true) {
+        var client_address: net.Address = undefined;
+        var client_address_len: posix.socklen_t = @sizeOf(net.Address);
+
+        const socket = posix.accept(listener, &client_address.any, &client_address_len, 0) catch |err| {
+            std.debug.print("error accept: {}\n", .{err});
+            continue;
+        };
+        defer posix.close(socket);
+
+        const timeout = posix.timeval{ .sec = 2, .usec = 500_000 };
+        try posix.setsockopt(socket, posix.SOL.SOCKET, posix.SO.RCVTIMEO, &std.mem.toBytes(timeout));
+
+        std.debug.print("{f} connected\n", .{client_address});
+
+        const read = posix.read(socket, &buf) catch |err| {
+            std.debug.print("error reading: {}\n", .{err});
+            continue;
+        };
+        if (read == 0) {
+            continue;
+        }
+        write(socket, buf[0..read]) catch |err| {
+            std.debug.print("error writing: {}\n", .{err});
+        };
+    }
+}
+
+fn write(socket: posix.socket_t, msg: []const u8) !void {
+    var pos: usize = 0;
+    while (pos < msg.len) {
+        const written = try posix.write(socket, msg[pos..]);
+        if (written == 0) {
+            return error.Closed;
+        }
+        pos += written;
+    }
+}
